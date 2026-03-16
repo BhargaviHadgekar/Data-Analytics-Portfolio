fb03adb7cc92bbd12831c2b3ff45e6218e91e136
BhargaviHadgekar <bghadgekar0812@gmail.com>
2026-03-16 18:53:34 +0530

    updated Main.py

--- Student_Database_System/Main.py
+++ Student_Database_System/Main.py
@@ -17,9 +17,9 @@ def main():
         print("2. View Students")
         print("3. Update Student Grade")
         print("4. Delete Student Courses")
-        print("5.Course Averages")
+        print("5. Course Averages")
         print("6. Top Performars")
-        print("7.Exit")
+        print("7. Exit")
 
         choice = input("Enter choice number: " )
 
@@ -30,7 +30,7 @@ def main():
         elif choice == "3":
             Updater.update_grade()
         elif choice == "4":
-            deletor.delete_student_courses()
+            deletor.delete_student()
         elif choice == "5":
             report.course_averages()
         elif choice == "6":
