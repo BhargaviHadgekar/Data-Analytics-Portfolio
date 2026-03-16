29b4e8e9589ab9a7aace440cfe7697f165be2328
BhargaviHadgekar <bghadgekar0812@gmail.com>
2026-03-16 16:03:53 +0530

    Fix folder name, add __init__.py, update test imports for CI

--- Student_Database_System/test_student_grade.py
+++ Student_Database_System/test_student_grade.py
@@ -1,11 +1,18 @@
+
+
 import unittest
-from connector import DBConnector
+
+
+from Student_Database_System.connector import DBConnector
+from Student_Database_System.create import StudentCreator
+from Student_Database_System.Delete import StudentDeleter
+from Student_Database_System.Update import GradeUpdater
+from Student_Database_System.Reports import Reports
 
 
 class TestStudentGrade(unittest.TestCase):
 
     def test_normalize_course(self):
-
         self.assertEqual(DBConnector.normalize_course("python"), "Python")
         self.assertEqual(DBConnector.normalize_course("JAVA"), "Java")
         self.assertEqual(DBConnector.normalize_course("C++"), "C++")
@@ -13,7 +20,6 @@ class TestStudentGrade(unittest.TestCase):
         self.assertIsNone(DBConnector.normalize_course("Ruby"))
 
     def test_validate_email(self):
-
         self.assertTrue(DBConnector.validate_email("bhargavi@gmail.com"))
         self.assertFalse(DBConnector.validate_email("bhargavi@yahoo.com"))
         self.assertFalse(DBConnector.validate_email("wrongemail"))
