9ee895862774f08e02b8f6b263197440fb0e3b6f
BhargaviHadgekar <bghadgekar0812@gmail.com>
2026-03-30 20:22:33 +0530

    Pytest

---
+++ test_Student.py
@@ -0,0 +1,14 @@
+import unittest
+from Student_Grades_system import GradeSystem
+class TestGradeSystem(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.grade_system = GradeSystem()
+        cls.grade_system.cursor.execute("DELETE FROM Grades")
+        cls.grade_system.cursor.execute("DELETE FROM Students")
+        cls.grade_system.cursor.execute("DELETE FROM Courses")
+        cls.grade_system.conn.commit()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.grade_system.conn.close()
