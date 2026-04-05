6b363a63c9d097684beec283acb825591783f756
BhargaviHadgekar <bghadgekar0812@gmail.com>
2026-04-06 02:59:08 +0530

    CICD PIPELINE

--- Inventory_system/Inventory.py
+++ Inventory_system/Inventory.py
@@ -171,4 +171,34 @@ if __name__ == "__main__":
             break
         else:
             print("Invalid choice! Please try again.")
-            print("-----------------------------")
+
+            
+
+while True:
+        print("Welcome to the Inventory Management System")
+        print("1. View Inventory")
+        print("2. Update Quantity")
+        print("3. Delete Product")
+        print("4. Sell Product")
+        print("5. Sales Records")
+        print("6. Total Sales")
+        print("7. Exit")
+        choice = input("Enter your choice: ")
+        if choice == '1':
+            view_inventory()
+        elif choice == '2':
+            update_quantity()
+        elif choice == '3':
+            delete_product()
+        elif choice == '4':
+            sell_product()
+        elif choice == '5':
+            view_sales_records()
+        elif choice == '6':
+            total_sales()
+        elif choice == '7':
+            break
+        else:
+            print("Invalid choice! Please try again.")
+
+                        
